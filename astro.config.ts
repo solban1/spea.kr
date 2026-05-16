60d433b8ed7e8f26f6f370abdac39157e8ffba0d
solban1 <solban1@daum.net>
2026-05-16 15:31:12 +0900

    feat: SEO

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,15 @@
 import type { AstroUserConfig } from "astro";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 const config: AstroUserConfig = {
   site: "https://spea.kr",
+  integrations: [
+    sitemap({
+      lastmod: new Date(),
+      priority: 1,
+    }),
+  ],
 };
 
 export default config;
